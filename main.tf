2855f30f4fe12490e24ce20248748bd39fcd849c
Ubuntu <ubuntu@ip-172-31-1-173.eu-west-1.compute.internal>
2020-09-07 14:01:08 +0000

    Cleaning up

--- main.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_instance" "sample" {
-  ami           = var.ec2_ami_id
-  instance_type = var.ec2_instance_type
-
-  tags = {
-    Name = var.ec2_instance_name
-  }
-}
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ec2_instance_id" {
-  value = aws_instance.sample.id
-  description = "This is the AWS ID for the instance you created"
-}
