59207d04ca886958ab01374142e947a2ea6a1684
Cobus Bernard <cobusb@amazon.com>
2020-09-08 12:27:07 +0000

    New version again

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "aws_instance" "sample" {
     Name = var.ec2_instance_name
   }
 }
+
+resource "aws_instance" "sample_2" {
+  ami           = var.ec2_ami_id
+  instance_type = var.ec2_instance_type
+
+  tags = {
+    Name = var.ec2_instance_name
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ec2_instance_id" {
   value       = aws_instance.sample.id
   description = "The AWS ID of the EC2 instance we created."
 }
+
+output "ec2_instance_id_2" {
+  value       = aws_instance.sample_2.id
+  description = "The AWS ID of the EC2 instance #2 we created."
+}
